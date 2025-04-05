d1d5102ddbf5288e8b8accdb9b4b2f93c3bdecfe
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-04-05 19:28:46 +0530

    sixth

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import child from "./child";
+import Child from "./child";
 import {useState} from "react"
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
     <div className="parent">
       <h1>Parent Component</h1>
       <p>{value}</p>
-      <child value = {value} setValue={setValue}></child>
+      <Child value = {value} setValue={setValue}></Child>
         {/* Do not remove the main div */}
     </div>
   )
